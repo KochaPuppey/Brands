b6e2debe8911788258607207edfe23ae6338227c
Andrew Kochkin <kochkin_9@mail.ru>
2025-09-26 19:31:11 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+let popupContainer = document.querySelector('.popup_container');
+let brandsOpen = document.querySelector('.popup_button_open');
+let arrowRotate = document.querySelector('.popup_line')
+
+brandsOpen.addEventListener('click', function () {
+  if (popupContainer.classList.contains('popup-show')) {
+    popupContainer.classList.remove('popup-show');
+    brandsOpen.textContent = 'Показать все';
+    arrowRotate.classList.remove('active');
+  } else {
+    popupContainer.classList.add('popup-show');
+    brandsOpen.textContent = 'Скрыть';
+    arrowRotate.classList.add('active');
+  }
+});
+
+let swiperInstance
+
+function initSwiper() {
+  if (window.innerWidth <= 767 && !swiperInstance) {
+    swiperInstance = new Swiper ('.swiper', {
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  } else if (window.innerWidth > 767 && swiperInstance) {
+    swiperInstance.destroy(true,true);
+    swiperInstance = null;
+  }
+}
+initSwiper();
+window.addEventListener("resize", initSwiper);
